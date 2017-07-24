0ab1b94be7bb738aa80ffb6d6957d896fd179542
Balazs Varga <balazs.varga@wup.hu>
2017-07-24 18:35:57 +0200

    Optional use compile dependencies instead of runtime when generate pom

--- plugin/src/test/groovy/digital/wup/android_maven_publish/AndroidMavenPublishTest.groovy
+++ plugin/src/test/groovy/digital/wup/android_maven_publish/AndroidMavenPublishTest.groovy
@@ -19,12 +19,60 @@ package digital.wup.android_maven_publish
 import org.gradle.api.Project
 import org.gradle.api.UnknownDomainObjectException
 import org.gradle.api.internal.component.Usage
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4.class)
 class AndroidMavenPublishTest extends BaseTestCase {
+
+    @Ignore
+    @Test
+    public void integrationTest() {
+        //runGradle('2.4', '1.1.2', '21.1.1')
+        //runGradle('2.4', '1.2.0', '21.1.1')
+        //runGradle('2.5', '1.2.0', '21.1.1')
+        //runGradle('2.6', '1.2.0', '21.1.1')
+        //runGradle('2.7', '1.2.0', '21.1.1')
+        //runGradle('2.8', '1.2.0', '21.1.1')
+        //runGradle('2.9', '1.2.0', '21.1.1')
+        //runGradle('2.9', '1.3.0', '21.1.1')
+        //runGradle('2.4', '1.3.1', '21.1.1')
+        //runGradle('2.5', '1.3.1', '21.1.1')
+        //runGradle('2.6', '1.3.1', '21.1.1')
+        //runGradle('2.7', '1.3.1', '21.1.1')
+        //runGradle('2.8', '1.3.1', '21.1.1')
+        //runGradle('2.9', '1.3.1', '21.1.1')
+        //runGradle('2.9', '1.5.0', '21.1.1')
+        //runGradle('2.10', '1.5.0', '21.1.1')
+        //runGradle('2.11', '1.5.0', '21.1.1')
+        //runGradle('2.12', '1.5.0', '21.1.1')
+        //runGradle('2.13', '1.5.0', '21.1.1')
+        //runGradle('2.10', '2.0.0', '21.1.1')
+        //runGradle('2.11', '2.0.0', '21.1.1')
+        //runGradle('2.12', '2.0.0', '21.1.1')
+        //runGradle('2.13', '2.0.0', '21.1.1')
+        //runGradle('2.10', '2.1.0', '23.0.2')
+        //runGradle('2.11', '2.1.0', '23.0.2')
+        //runGradle('2.12', '2.1.0', '23.0.2')
+        //runGradle('2.13', '2.1.0', '23.0.2')
+        runGradle('2.14.1', '2.1.3', '23.0.3')
+        runGradle('2.14.1', '2.2.3', '23.0.3')
+        runGradle('3.3', '2.3.3', '25.0.3')
+    }
+
+    private void runGradle(String gradleVersion, String gradleToolsVersion, buildToolsVersion) {
+        GradleRunner runner = GradleRunner.create()
+                .withProjectDir(getIntTestDir())
+                .withArguments("-PgradleBuildToolsVersion=${gradleToolsVersion}",
+                "-PbuildToolsVersion=${buildToolsVersion}",
+                '-PmavenPublishVersion=1.0.0-SNAPSHOT', 'clean', 'build', 'publish')
+                .withGradleVersion(gradleVersion)
+
+        runner.build()
+    }
 
     @Test
     public void testMavenPublishPluginRequired() {
@@ -56,7 +104,7 @@ class AndroidMavenPublishTest extends BaseTestCase {
 
         assertTrue(artifact instanceof AarPublishArtifact)
 
-        assertTrue(usage.getDependencies().isEmpty())
+        assertFalse(usage.getDependencies().isEmpty())
     }
 
     @Test
--- plugin/src/test/groovy/digital/wup/android_maven_publish/BaseTestCase.groovy
+++ plugin/src/test/groovy/digital/wup/android_maven_publish/BaseTestCase.groovy
@@ -16,8 +16,8 @@
 
 package digital.wup.android_maven_publish
 
-import junit.framework.TestCase
 import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.testfixtures.ProjectBuilder
 
 import java.security.CodeSource
@@ -35,17 +35,38 @@ abstract class BaseTestCase extends GroovyTestCase {
         p.apply plugin: 'com.android.library'
         p.plugins.apply(AndroidMavenPublishPlugin.class)
 
+        p.repositories {
+            jcenter()
+        }
         p.android {
             compileSdkVersion 25
             buildToolsVersion "25.0.3"
+        }
+        p.dependencies {
+            compile 'com.google.code.gson:gson:2.8.1'
+        }
+
+        p.publishing {
+            publications {
+                mavenAar(MavenPublication) {
+                    from p.components.android
+
+                }
+            }
+            repositories {
+                maven {
+                    url mavenLocal().url
+                }
+            }
         }
         p.evaluate()
         return p
     }
 
+    @SuppressWarnings("GrMethodMayBeStatic")
     protected final Project buildJavaProject() {
         Project p = ProjectBuilder.builder()
-                    .build()
+                .build()
         p.apply plugin: 'java'
         p.apply plugin: AndroidMavenPublishPlugin
 
@@ -73,7 +94,25 @@ abstract class BaseTestCase extends GroovyTestCase {
         fail("Fail to get tests folder")
     }
 
+    protected final File getIntTestDir() {
+        CodeSource source = getClass().getProtectionDomain().getCodeSource()
+        if (source != null) {
+            URL location = source.getLocation();
+            try {
+                File dir = new File(location.toURI())
+                assertTrue(dir.getPath(), dir.exists())
+                File f = dir.getParentFile().getParentFile().getParentFile().getParentFile()
+
+                return new File(f, 'android-int-test')
+            } catch (URISyntaxException e) {
+                fail(e.getLocalizedMessage())
+            }
+        }
+        fail("Fail to get tests folder")
+    }
+
+
     protected final File getProjectAarOutputsDir() {
-        new File(getAndroidTestDir(), "build${File.separator}outputs")
+        new File(getAndroidTestDir(), "build${File.separator}outputs${File.separator}aar")
     }
 }
