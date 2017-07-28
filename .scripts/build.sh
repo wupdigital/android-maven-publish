be415bac2cbc0e6f7ed316c3ef6beddd1fc2daba
Balazs Varga <balazs.varga@wup.hu>
2017-07-28 11:26:59 +0200

    Update build script

---
+++ .scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Build
+./gradlew check jacocoTestReport coveralls
+
+if [ "$TRAVIS_REPO_SLUG" == "wupdigital/android-maven-publish" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+	# Publish the artifacts
+	./gradlew publish publishPlugins -Dgradle.publish.key=$GRADLE_KEY -Dgradle.publish.secret=$GRADLE_SECRET || exit 1
+	# Push the groovydoc
+	./gradlew publishGhPages || exit 1
+fi
