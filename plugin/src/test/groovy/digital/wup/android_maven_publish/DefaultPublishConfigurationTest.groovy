9bc87e30097510cf6a74d0f3a82ae324e39385a0
Balázs Varga <balazs.varga@wup.hu>
2018-06-03 21:39:01 +0200

    Add an empty unit test

---
+++ plugin/src/test/groovy/digital/wup/android_maven_publish/DefaultPublishConfigurationTest.groovy
@@ -0,0 +1,12 @@
+package digital.wup.android_maven_publish
+
+import org.junit.Assert
+import org.junit.Test
+
+class DefaultPublishConfigurationTest {
+
+    @Test
+    void testExample() {
+        Assert.assertTrue(true)
+    }
+}
