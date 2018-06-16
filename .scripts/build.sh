0fbdb0914110c08f42c83e7ce0605a6e2805886c
Balázs Varga <balazs.varga@wup.hu>
2018-06-16 14:38:01 +0200

    Support publishing on support branches and master too

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -5,7 +5,7 @@
 # Build plugin and publish test coverage
 ./gradlew build jacocoTestReport coveralls -c plugin/settings.gradle || exit 1
 
-if [ "$TRAVIS_REPO_SLUG" == "wupdigital/android-maven-publish" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "wupdigital/android-maven-publish" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && ([ "$TRAVIS_BRANCH" == "master" ] || [["$TRAVIS_BRANCH" == "support/*"]]); then
 	# Publish the artifacts
 	./gradlew publish publishPlugins -Dgradle.publish.key=$GRADLE_KEY -Dgradle.publish.secret=$GRADLE_SECRET -c plugin/settings.gradle || exit 1
 	# Push the groovydoc
